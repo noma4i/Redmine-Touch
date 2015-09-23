d2d60f0b694c3c693c1ef3b42edba821958157ef
Alexander Tsirel <noma4i@gmail.com>
2015-09-23 11:59:29 +1000

    Bump version

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_touch do
   name 'Touch'
   author 'Alex Tsirel'
   description 'Add functionality of *who updated this last*'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/noma4i/Redmine-Touch'
   author_url 'https://github.com/noma4i'
   requires_redmine :version_or_higher => '2.0.0'
